441157561c6d8a8912ec62b7b065bce022b086d0
nikita.kozurkins <boba@sosiski.com>
2025-07-12 18:12:39 +0300

    Removed unnecessary console.log statements and comments

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -5,12 +5,7 @@ import { StatusCodes } from 'http-status-codes'
 import { OrderDto } from './dto/order-dto'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
-  // Build and send a GET request to the server
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
-  // Log the response status, body and headers
-  console.log('response body:', await response.json())
-  console.log('response headers:', response.headers())
-  // Check if the response status is 200
   expect(response.status()).toBe(200)
 })
 
@@ -19,38 +14,28 @@ test('post order with correct data should receive code 201', async ({ request })
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: requestBody,
   })
-  console.log('response status:', response.status())
-  console.log('request body:', requestBody)
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
 test('get order with orderId 0 data should receive code 400', async ({ request }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0', {})
   const responseBody = await response.json()
-  console.log('response status:', response.status())
-  console.log('response body:', await response.json())
   expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST)
   expect.soft(responseBody.message).toBe('getById.id: must be greater than or equal to 1')
 })
 
 test('get order with orderId 11 data should receive code 400', async ({ request }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11', {})
-  console.log('response status:', response.status())
-  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
 test('get order with empty orderId data should receive code 400', async ({ request }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/', {})
-  console.log('response status:', response.status())
-  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
 })
 
 test('get order with string orderId data should receive code 400', async ({ request }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/test', {})
-  console.log('response status:', response.status())
-  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
@@ -58,7 +43,5 @@ test('post order with incorrect data type should receive code 415', async ({ req
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: 'string',
   })
-  console.log('response status:', response.status())
-  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
 })
